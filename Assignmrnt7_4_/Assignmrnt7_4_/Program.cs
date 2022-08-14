3b3b6b135d464fa881b2a1b95cab6a14930d81ce
Puja Samdder <pujasamadder05@gmail.com>
2022-08-14 10:01:59 +0530

    modified

--- Assignmrnt7_4_/Assignmrnt7_4_/Program.cs
+++ Assignmrnt7_4_/Assignmrnt7_4_/Program.cs
@@ -10,12 +10,14 @@ namespace Assignment7_4
         {
             try
             {
-                MiddleConnect.SetData(001, "Isan Bain", 10000);
-                MiddleConnect.SetData(002, "Dia Roy", 20000);
-                MiddleConnect.SetData(003, "ABC DEF", 30000);
+                MiddleConnect.SetData(001, "Rahul Sharma", 17000);
+                MiddleConnect.SetData(002, "Bibek Saha", 30000);
+                MiddleConnect.SetData(003, "Priyanka Roy", 40000);
+                MiddleConnect.SetData(004, "Aman Manak", 20000);
+                MiddleConnect.SetData(005, "Jasspreet Singh", 50000);
 
                 List<Manager> manager = MiddleConnect.Managers;
-                string path = @"C:\Users\user\Desktop\C#_Assignments\Assignment_7\Files\ManagerData\Manager.txt";
+                string path = @"C:\Users\RAJA\C_Sharp_Assignment\Files\Manager\Manager.txt";
                 FileStream fs = new FileStream(path, FileMode.Append, FileAccess.Write);
 
 
